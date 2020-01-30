4ead9c3b30ebf278fd4e173bf9e65d5b3efa8324
Md. Shamiul Islam <shamiul.islam@bs-23.net>
2020-01-30 14:43:10 +0600

    Travish added

--- GoogleMapTests/GoogleMapTests.swift
+++ GoogleMapTests/GoogleMapTests.swift
@@ -11,26 +11,20 @@ import XCTest
 
 class GoogleMapTests: XCTestCase {
     
+    var converter: Converter!
+
     override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      super.setUp()
+      converter = Converter()
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+      converter = nil
+      super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testConversionForOne() {
+      let result = converter.convert(1)
+      XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
